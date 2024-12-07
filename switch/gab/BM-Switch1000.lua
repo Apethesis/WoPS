b2b17bf8a3d1d8bb5b6135867a53dc920e6851ea
Gabi Casap <81907469+someoneidoknow@users.noreply.github.com>
2024-12-07 14:02:04 +0000

    ok

--- switch/gab/BM-Switch1000.lua
+++ switch/gab/BM-Switch1000.lua
@@ -2,42 +2,20 @@
 
 local Layer1 = require('../../layer1')
 local Layer2 = require('../../layer2')
-local DHCP = require('../../layer3/dchp.lua')
-local ip_decode = require('../../utility/ip_decode.lua')
 
 local port1 = GetPort(1)
 local connectedPorts = GetPartsFromPort(1, "Port")
-local Disk = GetDisk(1)
 
 local layer2Instances = {}
-local dhcpInstances = {}
 for _, port in connectedPorts do
     if port ~= port1 then
         local layer1Instance = Layer1.new(port)
         layer1Instance:addBlacklistPort(port1) -- ignore port 1 so the requests dont come 4 times or depending on the number of ports
         local layer2Instance = Layer2.new(layer1Instance)
         layer2Instances[port] = layer2Instance
-
-        -- Create DHCP instance for each Layer2 instance
-        local dhcpInstance = DHCP.new(layer2Instance)
-        dhcpInstances[port] = dhcpInstance
     end
 end
 local macTable = {} -- { [macAddress] = { layer2 = Layer2Instance, timestamp = number } }
-local ipLeaseTable = {} -- { [ipAddress] = { macAddress = number, leaseExpiry = number } }
-
--- Load IP lease table from Disk
-local function loadIpLeaseTable()
-    local data = Disk:Read("ipLeaseTable")
-    if data then
-        ipLeaseTable = textutils.unserialize(data) or {}
-    end
-end
-
--- Save IP lease table to Disk
-local function saveIpLeaseTable()
-    Disk:Write("ipLeaseTable", textutils.serialize(ipLeaseTable))
-end
 
 -- Packet forwarding function
 local function forwardPacket(receivingLayer2, packet)
@@ -88,56 +66,3 @@ for _, layer2 in pairs(layer2Instances) do
         forwardPacket(layer2, layer2:decode(packet))
     end)
 end
-
--- DHCP server functions
-local function handleDhcpDiscover(dhcpInstance, clientMAC)
-    local availableIp = nil
-    for _, ip in ipairs(ip_decode.generateIpList("192.168.1.0/24")) do
-        if not ipLeaseTable[ip] or os.time() > ipLeaseTable[ip].leaseExpiry then
-            availableIp = ip
-            break
-        end
-    end
-
-    if availableIp then
-        local leaseTime = 3600 -- 1 hour lease time
-        local leaseExpiry = os.time() + leaseTime
-        ipLeaseTable[availableIp] = { macAddress = clientMAC, leaseExpiry = leaseExpiry }
-        saveIpLeaseTable()
-        dhcpInstance:offer(clientMAC, ip_decode.ipToNumber(availableIp), leaseTime)
-    else
-        dhcpInstance:nak(clientMAC)
-    end
-end
-
-local function handleDhcpRequest(dhcpInstance, clientMAC, requestedIp)
-    local ipStr = ip_decode.numberToIp(requestedIp)
-    if ipLeaseTable[ipStr] and ipLeaseTable[ipStr].macAddress == clientMAC then
-        local leaseTime = 3600 -- 1 hour lease time
-        local leaseExpiry = os.time() + leaseTime
-        ipLeaseTable[ipStr].leaseExpiry = leaseExpiry
-        saveIpLeaseTable()
-        dhcpInstance:ack(clientMAC, requestedIp, leaseTime)
-    else
-        dhcpInstance:nak(clientMAC)
-    end
-end
-
--- Connect to DHCP events
-for _, dhcpInstance in pairs(dhcpInstances) do
-    dhcpInstance.EVENT:Connect(function(ip, leaseExpiry)
-        print("Assigned IP:", ip_decode.numberToIp(ip), "Lease Expiry:", leaseExpiry)
-    end)
-
-    dhcpInstance.layer2.ListenEVENT:Connect(function(sender, packet)
-        local msgType, clientMAC, ip, leaseExpiry = dhcpInstance:decodePacket(packet.data)
-        if msgType == DHCP.DHCP_DISCOVER then
-            handleDhcpDiscover(dhcpInstance, clientMAC)
-        elseif msgType == DHCP.DHCP_REQUEST then
-            handleDhcpRequest(dhcpInstance, clientMAC, ip)
-        end
-    end)
-end
-
--- Load IP lease table on startup
-loadIpLeaseTable()
